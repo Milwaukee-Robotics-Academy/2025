6abce533467309b2e84ed0227388744c3dd65b3a
Laptop4 <Laptop4@milwaukeeroboticsacademy.org>
2025-02-01 15:19:29 -0600

    1st Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -188,7 +188,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("Right Start Auto");
   }
 
   public void setMotorBrake(boolean brake)
