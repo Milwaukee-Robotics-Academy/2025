6715b00133091e6201472dc19e370f7460dd8360
Erik Lee <ErikSLee@gmail.com>
2025-03-14 06:13:49 -0500

    10287 (#33)
    * vendordeps (#25)
    
    * Auto chooser
    
    * vendordeps
    
    * delete example code
    
    * correct zeroing
    
    * team number
    
    * vision and autos (#31)
    
    * readme
    
    * Feature/vision (#29)
    
    * 7
    
    * bumpers (#6)
    
    * zero alliance
    
    * driving cleanup
    
    * Add PathPlanner Auto w/NamedCommands
    
    * no auto
    
    * vision updates
    
    * cameras
    
    * cam location
    
    * Partially working vision
    
    * degrees
    
    * yagsl config update
    
    * yagsl config update
    
    * vision
    
    * Update Vision.java
    
    * period
    
    * camera location
    
    * vision counter
    
    * cleanup
    
    * auto work
    
    * max_speed
    
    * cleanup
    
    * readying to merge to main
    
    * Update README and add AlgaeEndEffector subsystem implementation
    
    ---------
    
    Co-authored-by: Laptop7 <laptop7@milwaukeeroboticsacademy.org>
    Co-authored-by: MRALaptop7 <113262897+MRALaptop7@users.noreply.github.com>
    Co-authored-by: Laptop 9 <laptop9@milwaukeeroboticsacademy.org>
    
    ---------
    
    Co-authored-by: Laptop 9 <laptop9@milwaukeeroboticsacademy.org>
    Co-authored-by: Laptop7 <laptop7@milwaukeeroboticsacademy.org>
    Co-authored-by: MRALaptop7 <113262897+MRALaptop7@users.noreply.github.com>
    
    * Algae
    
    ---------
    
    Co-authored-by: MRALaptop9 <113261996+MRALaptop9@users.noreply.github.com>
    Co-authored-by: Laptop 9 <laptop9@milwaukeeroboticsacademy.org>
    Co-authored-by: Laptop7 <laptop7@milwaukeeroboticsacademy.org>
    Co-authored-by: MRALaptop7 <113262897+MRALaptop7@users.noreply.github.com>
    Co-authored-by: Laptop 5 <laptop5@milwaukeeroboticsacademy.org>

--- src/main/java/frc/robot/subsystems/AlgaeEndEffector.java
+++ src/main/java/frc/robot/subsystems/AlgaeEndEffector.java
@@ -125,10 +125,11 @@ public class AlgaeEndEffector extends SubsystemBase {
     return new InstantCommand(this::stopIntake, this).withName("StopIntake");
   }
   public Command goUpFunctionCommand(){
-    return new RunCommand(this::raiseArm, this).withName("RaiseArm");
+    return this.startEnd(this::raiseArm,this::stopArm);
+    //return new RunCommand(this::raiseArm, this).withName("RaiseArm");
   }
   public Command goDownFunctionCommand(){
-    return new RunCommand(this::lowerArm, this).withName("LowerArm");
+    return this.startEnd(this::lowerArm,this::stopArm);
   }
     
   public Command stop2Command(){
