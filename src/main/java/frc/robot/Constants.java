f38ee80ffc8de8f8eda31b89957877fe463b7d7b
Laptop4 <Laptop4@milwaukeeroboticsacademy.org>
2025-03-24 10:19:15 -0500

    camera locations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -61,21 +61,21 @@ public final class Constants {
           Units.inchesToMeters(11.75), 
           Units.inchesToMeters(9), 
           Units.inchesToMeters(8)),
-      new Rotation3d(0, 0, Units.degreesToRadians(-16)));
+      new Rotation3d(0, 0, Units.degreesToRadians(-15)));
     public static final Transform3d kRobotToCamRight = new Transform3d(
       new Translation3d(
-        Units.inchesToMeters(11.75), 
+        Units.inchesToMeters(-11.75), 
         Units.inchesToMeters(-9), 
         Units.inchesToMeters(8)),
-        new Rotation3d(0, 0, Units.degreesToRadians(16)));
+        new Rotation3d(0, 0, Units.degreesToRadians(172)));
     // The layout of the AprilTags on the field
     public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
 
     // The standard deviations of our vision estimated poses, which affect
     // correction rate
     // (Fake values. Experiment and determine estimation noise on an actual robot.)
-    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(8, 8, 40);
-    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(5, 5, 20);
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(8, 8, 12);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(5, 5, 10);
   }
 
   public static class OperatorConstants {
