ff0f0678addb775aa4ad7cdbf14445020235b9e2
MRALaptop3 <113261705+MRALaptop3@users.noreply.github.com>
2025-04-02 09:08:27 -0500

    vision update

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -60,7 +60,7 @@ public class Vision {
       SmartDashboard.putNumber(visionLabel + "/x", est.estimatedPose.getTranslation().getX());
       SmartDashboard.putNumber(visionLabel + "/y", est.estimatedPose.getTranslation().getY());
       SmartDashboard.putNumber(visionLabel + "/angle", est.estimatedPose.toPose2d().getRotation().getDegrees());
-      SmartDashboard.putNumber(visionLabel + "/posetimestamp", est.timestampSeconds);
+      //SmartDashboard.putNumber(visionLabel + "/posetimestamp", est.timestampSeconds);
     });
   }
   
