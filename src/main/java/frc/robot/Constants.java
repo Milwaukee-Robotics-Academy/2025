a3acfe835ce6c679444079dd81b5598de8d8ac9c
MRALaptop3 <113261705+MRALaptop3@users.noreply.github.com>
2025-04-02 13:50:43 -0500

    vision config

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -55,7 +55,7 @@ public final class Constants
     public static final Transform3d kRobotToCamBack = new Transform3d(
       new Translation3d(
           Units.inchesToMeters(-11.75), 
-          Units.inchesToMeters(-9), 
+          Units.inchesToMeters(9), 
           Units.inchesToMeters(8)),
       new Rotation3d(0, 0, Units.degreesToRadians(172)));
     public static final Transform3d kRobotToCamFront = new Transform3d(
